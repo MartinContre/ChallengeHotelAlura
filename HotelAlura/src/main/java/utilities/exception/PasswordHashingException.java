b19faf55afe8cb9a19ba2355e1d5244c3a35deb1
MartinContre <yoztacontreras@outlook.com>
2023-08-29 13:38:26 -0600

    Add exception

---
+++ HotelAlura/src/main/java/utilities/exception/PasswordHashingException.java
@@ -0,0 +1,9 @@
+package utilities.exception;
+
+public class PasswordHashingException extends RuntimeException {
+
+    public PasswordHashingException(String message) {
+        super(message);
+    }
+
+}
